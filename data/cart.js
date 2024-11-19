1be3c337c64c54ebb737ae7895ec2f631a7b9611
kkkcatt <973079247@qq.com>
2024-11-19 14:08:09 +0800

    genetrate delivery options HTML

--- data/cart.js
+++ data/cart.js
@@ -4,9 +4,11 @@ if (!cart) {
 	cart = [{
 		productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
 		quantity:2,
+		deliveryOptionId: '1'
 	},{
 		productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-		quantity:1
+		quantity:1,
+		deliveryOptionId: '2'
 	}];
 }
 
@@ -29,6 +31,7 @@ export function addToCart (productId) {
 		cart.push({
 			productId,
 			quantity,
+			deliveryOptionId : '1'
 		})
 	}
 	saveToStorage();
@@ -47,3 +50,23 @@ export function removeFromCart(productId) {
 	saveToStorage()
 }
 
+export function calculateCartQuantity() {
+	let cartQuantity = 0;
+	cart.forEach((cartItem) => {
+		if(cartItem){
+		cartQuantity += cartItem.quantity
+	}
+	})
+	return cartQuantity;
+ }
+
+ export function  updateQuantity(productId, newQuantity) {
+	let matchingItem;
+	cart.forEach((cartItem) => {
+		if(productId === cartItem.productId) {
+			matchingItem = cartItem
+		}
+	})
+	matchingItem.quantity = newQuantity;
+	saveToStorage()
+ }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,9 @@
-import {cart,addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js'
 let productHtml ='';
 
+updateCartQuantity()
 products.forEach((product) => {
 	productHtml += `
 		<div class="product-container">
